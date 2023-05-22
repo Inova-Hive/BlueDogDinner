5281fd8de75b79134d76414dfe747a46a8974708
Brian Lara <Briandlara1@gmail.com>
2023-05-22 14:43:09 -0400

    made changes to my component structure

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Nav: React.FC = () => {
     return (
-      <div className="text-center pt-6 pb-4 navbar-shadow">
-      <Link to="/" className="text-4xl font-1-bold text-custom-red mb-6">BLUE DOG DINER</Link>
-      <div className="space-x-16 mt-4">
-      <Link to="/events" className="font-1-semibold text-md text-custom-red hover:text-custom-red-hover transition-colors duration-700">EVENTS</Link>
-            <Link to="/menu" className="font-1-semibold text-md text-custom-red hover:text-custom-red-hover transition-colors duration-700">MENU</Link>
-            <Link to="/order" className="font-1-semibold text-md text-custom-red hover:text-custom-red-hover transition-colors duration-700">ORDER ONLINE</Link>
-            <Link to="/contact" className="font-1-semibold text-md text-custom-red hover:text-custom-red-hover transition-colors duration-700">CONTACT US</Link>
-      </div>
-    </div>
-      );
+        <div className="relative">
+            <div className="absolute left-10 top-6 ml-4 z-10">
+                <Link to="/">
+                    <img src="https://i.imgur.com/hYRkYh0.png" alt="Logo" className="h-20 w-20" /> 
+                </Link>
+            </div>
+            <div className="text-center pt-6 pb-4 navbar-shadow">
+                <Link to="/" className="text-4xl font-1-bold text-custom-red mb-6">BLUE DOG DINER</Link>
+                <div className="space-x-16 mt-4">
+                    <Link to="/events" className="font-1-semibold text-md text-custom-red hover:text-custom-red-hover transition-colors duration-700">EVENTS</Link>
+                    <Link to="/menu" className="font-1-semibold text-md text-custom-red hover:text-custom-red-hover transition-colors duration-700">MENU</Link>
+                    <Link to="/order" className="font-1-semibold text-md text-custom-red hover:text-custom-red-hover transition-colors duration-700">ORDER ONLINE</Link>
+                    <Link to="/contact" className="font-1-semibold text-md text-custom-red hover:text-custom-red-hover transition-colors duration-700">CONTACT US</Link>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Nav
+export default Nav;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import Banner from '../components/Banner'
+import Banner from '../components/Banner';
+import Info from '../components/Info';
 import { Link } from 'react-router-dom';
 
 
@@ -7,21 +8,8 @@ const Home: React.FC = () => {
   return(
     <div>
         <Banner/>
-        <div className="text-center pt-6 pb-4 mx-auto w-2/5 mb-32">
-          <h2 className="text-3xl font-1-bold text-custom-red mt-24 mb-6">Why Blue Dog Diner's Hot Dogs are the Best in Town</h2>
-          <p className="text-xl font-1-bold text-custom-red">Our hot dogs are made with 100% organic ingredients and are grilled to perfection.</p>  
-          
-        </div>
+        <Info />
         
-        <div className="flex items-center justify-center gap-32">
-          <img className="w-96 h-60" src="https://i.imgur.com/bZOGUn8.jpg" alt="Hot Dogs"/> {/* Replace with your actual Imgur link */}
-          <div className="flex flex-col justify-between items-center  h-full w-1/4">
-            <p className="text-xl font-1-bold text-custom-red text-center"> We offer over 20 different types of hot dogs, each with its unique flavor and ingredients.</p>
-            <Link to="/menu" className="font-1-semibold px-10 py-2 m-6 border-8 border-custom-red text-xl text-custom-red hover:bg-custom-red hover:text-white transition-colors duration-500">
-              <button>Check our Menu</button>
-            </Link>
-          </div>
-        </div>
     </div>
   ) 
 }
