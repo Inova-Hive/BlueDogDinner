b184c4ecfdd9b51a982a9e4ba2f2d08234d4ea5b
Brian Lara <Briandlara1@gmail.com>
2023-06-02 21:37:14 -0400

    updated css

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,16 +1,21 @@
 import React, { MouseEvent } from 'react';
 import { GoogleLogin, useGoogleLogin } from '@react-oauth/google';
 import axios from 'axios';
+
 import { BASE_URL } from '../globals';
+
 const Login: React.FC = () => {
   const handleGoogleLogin = async (response: any) => {
     try {
-      const data = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
+      const { data } = await axios.get('https://www.googleapis.com/oauth2/v3/userinfo', {
         headers: {
           "Authorization": `Bearer ${response.access_token}`
         }
+        
       });
-      console.log(data);
+      const userResponse = await axios.post(`${BASE_URL}/auth/session-token`, data);
+      // Now userResponse should contain the user data from your own server
+      console.log(userResponse);
     } catch (err) {
       console.log(err);
     }
@@ -21,10 +26,6 @@ const Login: React.FC = () => {
   return (
     <div className="max-w-3xl mx-auto px-4 py-2 flex flex-col items-center justify-center" style={{ height: 'calc(100vh - 280px)' }}>
       <p className="text-2xl font-1-semibold text-custom-blue mb-10">Please login to proceed.</p>
-      {/* <GoogleLogin
-        onSuccess={handleLoginSuccess}
-        onError={handleLoginError}
-      /> */}
       <header className='Test'>
         <button onClick={(event: React.MouseEvent<HTMLButtonElement>) => login()}>
           <i className="fa-brands fa-google"></i>
