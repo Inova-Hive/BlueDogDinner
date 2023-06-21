37b13953a59e0812e30dd396e344eb4d765c712e
Brian Lara <Briandlara1@gmail.com>
2023-06-20 22:40:57 -0400

    added order online link to banner button

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Banner: React.FC = () => {
     return (
         <div className="bg-hero-image bg-center bg-cover flex items-center justify-center flex-col sm:flex-row text-center"  style={{ height: 'calc(100vh - 120px)' }}>
-            <Link to="/link1" className="font-1-semibold px-16 py-4 m-6 border-8 border-white text-2xl text-white bg-black bg-opacity-70 hover:bg-white hover:bg-opacity-95 hover:border-opacity-5 hover:text-black transition-colors duration-500">ORDER ONLINE</Link>
+            <Link to="https://www.toasttab.com/blu-dog-diner-3216-judson-st-unit-b/v3" className="font-1-semibold px-16 py-4 m-6 border-8 border-white text-2xl text-white bg-black bg-opacity-70 hover:bg-white hover:bg-opacity-95 hover:border-opacity-5 hover:text-black transition-colors duration-500">ORDER ONLINE</Link>
             <Link to="/menu" className="font-1-semibold px-16 py-4 m-6 border-8 border-white text-2xl text-white bg-black bg-opacity-70 hover:bg-white hover:bg-opacity-95 hover:border-opacity-5 hover:text-black transition-colors duration-500">SEE THE MENU</Link>
         </div>
     );
